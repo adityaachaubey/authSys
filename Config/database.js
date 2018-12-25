221a51702d0ed88b85c9e4ed975c865b062178fc
Adityaa Chaubey <adityaa.chaubey@gmail.com>
2018-12-26 01:20:27 +0530

    initial commit apis working

---
+++ Config/database.js
@@ -0,0 +1,10 @@
+
+// ec2 instance or Mlab link here
+//module.exports = {
+    //'db': 'mongodb://'
+//};
+
+// // local server
+ module.exports = {
+     'db':'mongodb://localhost:27017/authSys'
+ };
